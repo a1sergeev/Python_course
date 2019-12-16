f477691bdbac3a50af0aa332706f7961a3b17b42
Aleksandr <57106246+a1sergeev@users.noreply.github.com>
2019-12-16 13:25:41 +0300

    Update zp.py
    Исправил

--- zp.py
+++ zp.py
@@ -3,16 +3,20 @@
 В расчете необходимо использовать формулу: (выработка в часах*ставка в час) + премия.
 Для выполнения расчета для конкретных значений необходимо запускать скрипт с параметрами.
 '''
+from sys import argv
+hours = argv[1]
+rate = argv[2]
+percent = argv[3]
 
-def pay():
+def pay(hours, rate, percent):
     try:
-        hours = int(input('Выработка в часах: '))
-        rate = int(input('Cтавка работника в час: '))
-        bonus = (hours * rate) * int(input('Сколько процентов премии: '))/100
+        #hours = int(input('Выработка в часах: '))
+        #rate = int(input('Cтавка работника в час: '))
+        bonus = (hours * rate) * percent/100
         salary = hours * rate + bonus
         return f'{salary:.2f} rub'
     except ValueError:
         return f'Только числа! '
 
-if __name__ == '__main__':
-    print(pay())
+#if __name__ == '__main__':
+    #print(pay())
